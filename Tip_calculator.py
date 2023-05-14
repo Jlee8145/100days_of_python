89298e01a61fe183e661820cf2583ef28b627417
Joshua R Lee <111541865+Jlee8145@users.noreply.github.com>
2023-05-13 20:40:21 -0400

    Add files via upload

--- Tip_calculator.py
+++ Tip_calculator.py
@@ -6,6 +6,7 @@ tip_as_percent = tip / 100
 total_tip_amount = bill * tip_as_percent
 total_bill = bill + total_tip_amount
 bill_per_person = total_bill / people
-total = round(bill_per_person, 2) 
+total = round(bill_per_person, 2)
+total = "{:.2f}".format(bill_per_person64)
 print(f"Each person should pay: ${total} Dollars!")
 
